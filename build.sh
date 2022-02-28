80b3e9deaccf24617882b995daf14f8b03805ecd
henry <henry.behrend@outlook.de>
2022-02-28 16:27:23 +0100

    Build script: Provided better log output and fixed an issue where a warning would be thrown by compiling a third time.

--- build.sh
+++ build.sh
@@ -1,23 +1,40 @@
-INPUT_DIR=$PWD/src
-OUTPUT_DIR=$PWD/dist
-[ ! -d $OUTPUT_DIR ] && mkdir -p $OUTPUT_DIR
+INPUT_DIR="$PWD/src"
+OUTPUT_DIR="$PWD/dist"
+
+LATEX_RUN="pdflatex -interaction=nonstopmode -output-directory=$OUTPUT_DIR main.tex"
+BIBTEX_RUN="bibtex main.aux"
+
+[ ! -d "$OUTPUT_DIR" ] && mkdir -p "$OUTPUT_DIR"
+
 # Compile for the first time and generate AUX files for BibTeX.
 echo
 echo "*** Compiling for the first time ***"
-echo
-cd $INPUT_DIR
-pdflatex -output-directory=$OUTPUT_DIR main.tex | grep "LaTeX [(Warning)(Error)]"
+cd "$INPUT_DIR"
+OUTPUT=$($LATEX_RUN | grep "LaTeX [(Warning)(Error)]")
+if [ -z "$OUTPUT" ]; then
+    echo "Finished without warnings or errors."
+else
+    echo "$OUTPUT"
+fi
+
 # Generate bibliography from the generated AUX file.
 echo
-echo "*** Generating bibliography. ***"
+echo "*** Generating bibliography ***"
+cd "$OUTPUT_DIR"
+cp "$INPUT_DIR/sources.bib" .
+$BIBTEX_RUN
+
+# Compile three times. First to include the generated BBL file, second to take the included bibliography into account (page numbers, ...)
+# and a third time to get possibly changed labels and cross-references right.
 echo
-cd $OUTPUT_DIR
-cp $INPUT_DIR/sources.bib .
-bibtex main.aux
-# Compile twice. First to include the generated BBL file, second to take the included bibliography into account (page numbers, ...).
+echo "*** Compiling for the second time ***"
+cd "$INPUT_DIR"
+$LATEX_RUN &> /dev/null
+$LATEX_RUN &> /dev/null
+OUTPUT=$($LATEX_RUN | grep "LaTeX [(Warning)(Error)]")
+if [ -z "$OUTPUT" ]; then
+    echo "Finished without warnings or errors."
+else
+    echo "$OUTPUT"
+fi
 echo
-echo "*** Compiling for the second time. ***"
-echo
-cd $INPUT_DIR
-pdflatex -output-directory=$OUTPUT_DIR main.tex | grep "LaTeX [(Warning)(Error)]"
-pdflatex -output-directory=$OUTPUT_DIR main.tex | grep "LaTeX [(Warning)(Error)]"
