b959be3cf448c1dec8763c4dfca24c088dd596f1
Henry Behrend <henry.behrend@oeffentliche.de>
2022-03-01 07:22:04 +0100

    Added Windows support.

--- build.bat
+++ build.bat
@@ -1,23 +1,60 @@
-@echo OFF
+@echo off
 
-set ROOT_DIR=%cd%
-set SOURCE_DIR=%ROOT_DIR%\src
-set BUILD_DIR=%ROOT_DIR%\build
+set ROOT_DIR=%~dp0
+set SOURCE_DIR=%ROOT_DIR%src
+set BUILD_DIR=%ROOT_DIR%build
 
-if exist "%BUILD_DIR%" (
-	>nul 2>nul dir /a-d "%BUILD_DIR%\*" && (
+set LATEX_RUN=pdflatex -interaction=nonstopmode -file-line-error -synctex=1 -output-directory=%BUILD_DIR% main.tex
+set BIBTEX_RUN=bibtex main.aux
+
+if exist %BUILD_DIR% dir /a-d "%BUILD_DIR%" >nul 2>nul && (
 		
-		echo.
-		echo *** Recompiling ***
-		cd %SOURCE_DIR%
-		for /f %%i in ('dir') do set LOG=%%i
-		echo %LOG%
-	
+	@rem If there are cached files from previous compilations, reuse those.
+	echo.
+	echo *** Recompiling ***
+	cd "%SOURCE_DIR%"
+	%LATEX_RUN% >nul
+	findstr /c:"LaTeX Warning" /c:"LaTeX Error" "%BUILD_DIR%\main.log" > "%BUILD_DIR%\main.log.tmp" && (
+		type "%BUILD_DIR%\main.log.tmp"
+	) || (
+		echo Finished without warnings or errors.
 	)
-) else (
+	goto end
 
-	mkdir "%BUILD_DIR%"
-	
 )
 
-pause
+mkdir "%BUILD_DIR%"
+@rem Compile for the first time and generate AUX files for BibTeX.
+echo.
+echo *** Compiling for the first time ***
+cd "%SOURCE_DIR%"
+%LATEX_RUN% >nul
+findstr /c:"LaTeX Warning" /c:"LaTeX Error" "%BUILD_DIR%\main.log" > "%BUILD_DIR%\main.log.tmp" && (
+	type "%BUILD_DIR%\main.log.tmp"
+) || (
+	echo Finished without warnings or errors.
+)
+
+@rem Generate bibliography from the generated AUX file.
+echo.
+echo *** Generating bibliography ***
+cd "%BUILD_DIR%"
+copy "%SOURCE_DIR%\sources.bib" . >nul 2>nul
+%BIBTEX_RUN%
+
+@rem Compile three times. First to include the generated BBL file, second to take the included bibliography into account
+@rem (page numbers, ...) and a third time to get possibly changed labels and cross-references right.
+echo.
+echo *** Compiling for the second time ***
+cd "%SOURCE_DIR%"
+%LATEX_RUN% >nul
+%LATEX_RUN% >nul
+%LATEX_RUN% >nul
+findstr /c:"LaTeX Warning" /c:"LaTeX Error" "%BUILD_DIR%\main.log" > "%BUILD_DIR%\main.log.tmp" && (
+	type "%BUILD_DIR%\main.log.tmp"
+) || (
+	echo Finished without warnings or errors.
+)
+
+:end
+cd %ROOT_DIR%
--- clean.bat
+++ clean.bat
@@ -1,17 +1,18 @@
-rmdir /S build
-del *.aux
-del *.bbl
-del *.blg
-del *.lof
-del *.log
-del *.lot
-del *.gz
-del *.toc
-del *.run.xml
-del *blx.bib
-del *.pdf
-del *.bcf
-del *.bak
-del *.lol
-del *.out
-del *.tdo
+@echo off
+rmdir /q /s build
+@rem del *.aux
+@rem del *.bbl
+@rem del *.blg
+@rem del *.lof
+@rem del *.log
+@rem del *.lot
+@rem del *.gz
+@rem del *.toc
+@rem del *.run.xml
+@rem del *blx.bib
+@rem del *.pdf
+@rem del *.bcf
+@rem del *.bak
+@rem del *.lol
+@rem del *.out
+@rem del *.tdo
